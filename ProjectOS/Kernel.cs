484b8cfa4709c5fc0ed7c88779cac9ff515ebe64
Vitoludde <Vitoludde@gmail.com>
2021-01-11 16:40:55 +0100

    Add project files.

---
+++ ProjectOS/Kernel.cs
@@ -0,0 +1,50 @@
+﻿using Cosmos.System.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+using ProjectOS.SystemPrograms;
+
+namespace ProjectOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+
+            Console.WriteLine("Initializing FAT File System");
+            try
+            {
+                CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+                Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+                Console.WriteLine("Initialized FAT File System");
+            } catch (InvalidCastException e)
+            {
+                Console.WriteLine(String.Format("FATAL ERROR! Cannot Initialize FAT File System. Error: {0}", e));
+            }
+
+            Console.WriteLine("Starting File Handler");
+            try
+            {
+                SystemPrograms.FileHandler FileHandler = new SystemPrograms.FileHandler();
+                Console.WriteLine("Started File Handler");
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(String.Format("FATAL ERROR! Could not start File Handler. Error: {0}", e));
+            }
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+
+            
+        }
+    }
+}
---
+++ ProjectOS/SystemPrograms/FileHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace ProjectOS.SystemPrograms
+{
+    class FileHandler
+    {
+        void ReadFile(string Path)
+        {
+            try
+            {
+                var hello_file = Sys.FileSystem.VFS.VFSManager.GetFile(Path);
+                var hello_file_stream = hello_file.GetFileStream();
+
+                if (hello_file_stream.CanRead)
+                {
+                    byte[] text_to_read = new byte[hello_file_stream.Length];
+                    hello_file_stream.Read(text_to_read, 0, (int)hello_file_stream.Length);
+                    Console.WriteLine(Encoding.Default.GetString(text_to_read));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        void WriteFile(string PathToFile, string Filename, string Content)
+        {
+            try
+            {
+                var file = Sys.FileSystem.VFS.VFSManager.GetFile(PathToFile + "\\" + Filename);
+                if (file != null)
+                {
+                    var file_stream = file.GetFileStream();
+
+                    if (file_stream.CanWrite)
+                    {
+                        file_stream.Write(Encoding.ASCII.GetBytes(Content), 0, Content.Length);
+                    }
+                } else
+                {
+                    var newfile = Sys.FileSystem.VFS.VFSManager.CreateFile(PathToFile + "\\" + Filename);
+                    var newfile_stream = newfile.GetFileStream();
+
+                    if (newfile_stream.CanWrite)
+                    {
+                        newfile_stream.Write(Encoding.ASCII.GetBytes(Content), 0, Content.Length);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        void GetFiles(string Path)
+        {
+            var directory_list = Sys.FileSystem.VFS.VFSManager.GetDirectoryListing(Path);
+            foreach (var directoryEntry in directory_list)
+            {
+                Console.WriteLine(directoryEntry.mName);
+            }
+        }
+    }
+}
